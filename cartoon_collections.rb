c633a786c683a965eab6b1da9121eaab73b6ed4a
Val Osip <valosip@gmail.com>
2020-04-07 03:19:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)
   array.map { |string| string.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include? { |call| call.length > 4 }
+  
 end
 
 def find_the_cheese# code an argument here
