3f6198cf18e93d93c525430d1923d1a5b2889384
Val Osip <valosip@gmail.com>
2020-04-07 03:23:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.find { |cheese| cheese_types.include(cheese)
+  array.find { |cheese| cheese_types.include(cheese) }
 end
